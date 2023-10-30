a38fd419db17c05978ef6497ca8b8f61cff395e7
tggbco <vito.bica@gmail.com>
2023-10-30 15:28:15 -0700

    Updates to remove returns from template

--- Lab-11-Float/firmware/src/asmFmax.s
+++ Lab-11-Float/firmware/src/asmFmax.s
@@ -1,11 +1,11 @@
 /*** asmFmax.s   ***/
-
 #include <xc.h>
+.syntax unified
 
-// Declare the following to be in data memory
+@ Declare the following to be in data memory
 .data  
 
-// Define the globals so that the C code can access them
+@ Define the globals so that the C code can access them
 
 .global f1,f2,fMax,signBitMax,expMax,mantMax
 .type f1,%gnu_unique_object
@@ -26,21 +26,21 @@
 .type mant2,%gnu_unique_object
  
 .align
-# use these locations to store f1 values
+@ use these locations to store f1 values
 f1: .word 0
 sb1: .word 0
 biasedExp1: .word 0  /* the unmodified 8b exp value extracted from the float */
 exp1: .word 0
 mant1: .word 0
  
-# use these locations to store f2 values
+@ use these locations to store f2 values
 f2: .word 0
 sb2: .word 0
 exp2: .word 0
 biasedExp2: .word 0  /* the unmodified 8b exp value extracted from the float */
 mant2: .word 0
  
-# use these locations to store fMax values
+@ use these locations to store fMax values
 fMax: .word 0
 signBitMax: .word 0
 biasedExpMax: .word 0
@@ -51,26 +51,9 @@ mantMax: .word 0
 .type nanValue,%gnu_unique_object
 nanValue: .word 0x7FFFFFFF            
 
-# Tell the assembler that what follows is in instruction memory    
+@ Tell the assembler that what follows is in instruction memory    
 .text
 .align
-
-/* VB Comment: This next directive is required!
-// It allows the assembler to use both ARM 16b
-// and 32b instructions together. Without this
-// directive, the assembler will be limited to generating
-// 16b op-codes, and will give errors when specifying
-// arguments to instructions that will only work for
-// their 32b versions. Want proof? Remove this directive
-// and try to do a "push {r4-r11}" instruction. The
-// assembler will generate an error. The 16b version
-// of the "push" instruction
-// can only deal with registers r0-r7, so specifying
-// a register outside that range causes the assembler
-// to fail.
-*/
- 
-.syntax unified
 
 /********************************************************************
  function name: getSignBit
@@ -87,7 +70,7 @@ getSignBit:
 
     /* YOUR getSignBit CODE ABOVE THIS LINE! Don't forget to push and pop! */
     
-MOV pc,lr  /* getSignBit return to caller */
+
     
 /********************************************************************
  function name: getExponent
@@ -113,7 +96,7 @@ getExponent:
     
     /* YOUR getExponent CODE ABOVE THIS LINE! Don't forget to push and pop! */
    
-MOV pc,lr  /*  getExponent return to caller */
+
     
 /********************************************************************
  function name: getMantissa
@@ -130,7 +113,7 @@ getMantissa:
     
     /* YOUR getMantissa CODE ABOVE THIS LINE! Don't forget to push and pop! */
    
-MOV pc,lr  /*  getExponent return to caller */
+
 
     
 /********************************************************************
@@ -161,40 +144,15 @@ where:
 .type asmFmax,%function
 asmFmax:   
 
-    # save the caller's registers, as required by the ARM calling convention
-    push {r4-r11,LR}
     
-    /* Placeholder code: 
-     * STUDENTS: Make sure to disable this code by changing the
-     * .if statement from 0 to 1. Then scroll down and add your
-     * own code between the comments below.
-     */
-.if 1    
-    # just return r0 for now in the global
-    LDR r2,=fMax
-    STR r0,[r2]
-    # now return the address of fMax
-    LDR r0,=fMax
-    /* Add a conditional here just to make sure assembler options
-     * are correctly set up to auto-generate the IT instructions.
-     * If the assembler complains, make sure the following option
-     * is added to the compiler options line in XC32 project properties:
-     *     -Wa,-mimplicit-it=always
-     */
-    CMP r0,r0 /* guarantee that they are equal */
-    ADDNE r2,r2,r2 /* see if the assembler complains on this line */
-.endif
-    /* END: Placeholder code */
-
     /* YOUR asmFmax CODE BELOW THIS LINE! VVVVVVVVVVVVVVVVVVVVV  */
     
     
     /* YOUR asmFmax CODE ABOVE THIS LINE! ^^^^^^^^^^^^^^^^^^^^^  */
 
     # restore the caller's registers, as required by the ARM calling convention
-    pop {r4-r11,LR}
-
-    mov pc, lr	 /* asmFmax return to caller */
+    
+    /* asmFmax return to caller */
    
 
 /**********************************************************************/   
